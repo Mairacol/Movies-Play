84bb4f7e0ece4a5ba4ee55d18e13f246fa52640a
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-10-21 23:31:10 -0300

    details

--- src/navigation.js
+++ src/navigation.js
@@ -98,6 +98,11 @@ function movieDetailsPage() {
   categoriesPreviewSection.classList.add('inactive');
   genericSection.classList.add('inactive');
   movieDetailSection.classList.remove('inactive');
+
+// '#movie' , 'movieId' 
+const [_, movieId] = location.hash.split('=');
+getMovieById(movieId);
+ 
 }
 
 function searchPage() {
